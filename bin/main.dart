1fd85af57c2578398c1326884f7188222b5dda2e
sarbagya <sarbagyastha@gmail.com>
2019-05-19 08:43:45 +0545

    FIxed current datetime retrieval mistake

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,12 @@
 import 'package:nepali_utils/nepali_utils.dart';
 
 main(List<String> arguments) {
-  /*heading('Nepali Date Time');
+  heading('Nepali Date Time');
   NepaliDateTime gorkhaEarthQuake = NepaliDateTime.parse('2072-01-12T11:56:25');
   print(
       'Gorkha Earquake Details\nYear = ${gorkhaEarthQuake.year}\nMonth = ${gorkhaEarthQuake.month}\nDay = ${gorkhaEarthQuake.day}\nHour = ${gorkhaEarthQuake.hour}\nMinute = ${gorkhaEarthQuake.minute}');
   print('\nCurrent NepaliDateTime = ${NepaliDateTime.now()}');
-  */
+  
 
   heading('Date Converter');
   NepaliDateTime nt = DateConverter.toBS(DateTime(2019, 5, 19));
@@ -22,13 +22,11 @@ main(List<String> arguments) {
   var date3 = NepaliDateFormatter("h:mm a");
   var date4 = NepaliDateFormatter("hh 'o''clock' aa");
   var date5 = NepaliDateFormatter("yyyy.MMMM.dd GGG hh:mm a");
-  /*
   print(date1.format(gorkhaEarthQuake));
   print(date2.format(gorkhaEarthQuake));
   print(date3.format(gorkhaEarthQuake));
   print(date4.format(gorkhaEarthQuake));
   print(date5.format(gorkhaEarthQuake));
-  */
 
   heading('Nepali Number');
   print('123456 -> ${NepaliNumber.from(123456)}');
