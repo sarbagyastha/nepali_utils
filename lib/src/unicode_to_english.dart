b128e93f41832d856426e6f805931a510f07c1fe
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-13 02:12:25 +0545

    conversion for devnagari alphabets, vowels and numbers

---
+++ lib/src/unicode_to_english.dart
@@ -0,0 +1,97 @@
+// Copyright 2020 Sarbagya Dhaubanjar. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+/// Converts Nepali Unicode (Devnagari Literals) into English(Roman) literals.
+class UnicodeToEnglish {
+  static String _text = '';
+
+  /// Converts specifies [text] into english literals.
+  ///
+  /// if live is true, texts will convert in live manner.
+  /// i.e. as you go on typing
+  /// Default for live is false.
+  static String convert(String text, {bool live = false}) {
+    var splittedString = text.split('');
+    var convertedString = <String>[];
+    for (var i = 0; i < splittedString.length; i++) {
+      if (i < splittedString.length - 1 && splittedString[i + 1] == '\u094D') {
+        var fullStr = _unicodeMap[splittedString[i]].toString();
+        fullStr = fullStr.substring(0, fullStr.length - 1);
+        convertedString.add(fullStr);
+      } else if (splittedString[i] != '\u094D') {
+        convertedString
+            .add(_unicodeMap[splittedString[i]] ?? splittedString[i]);
+      }
+    }
+    _text = convertedString.join('');
+    return _text;
+  }
+}
+
+Map<String, String> get _unicodeMap => {
+      //ka - gya
+      '\u0915': 'ka',
+      '\u0916': 'kha',
+      '\u0917': 'ga',
+      '\u0918': 'gha',
+      '\u0919': 'ng',
+      '\u091A': 'ca',
+      '\u091B': 'cha',
+      '\u091C': 'ja',
+      '\u091D': 'jha',
+      '\u091E': 'ya',
+      '\u091F': 'Ta',
+      '\u0920': 'Tha',
+      '\u0921': 'Da',
+      '\u0922': 'Dha',
+      '\u0923': 'na',
+      '\u0924': 'ta',
+      '\u0925': 'tha',
+      '\u0926': 'da',
+      '\u0927': 'dha',
+      '\u0928': 'na',
+      '\u092A': 'pa',
+      '\u092B': 'fa',
+      '\u092C': 'ba',
+      '\u092D': 'bha',
+      '\u092E': 'ma',
+      '\u092F': 'ya',
+      '\u0930': 'ra',
+      '\u0932': 'la',
+      '\u0935': 'wa',
+      '\u0936': 'sha',
+      '\u0937': 'sha',
+      '\u0938': 'sa',
+      '\u0939': 'ha',
+      '\u0915\u094D\u0937': 'xa',
+      '\u0924\u094D\u0930': 'tra',
+      '\u091C\u094D\u091E': 'gya',
+      // a - ahm
+      '\u0905': 'a',
+      '\u0906': 'aa',
+      '\u0907': 'i',
+      '\u0908': 'ii',
+      '\u0909': 'u',
+      '\u090A': 'oo',
+      '\u090F': 'e',
+      '\u0910': 'ai',
+      '\u0913': 'o',
+      '\u0914': 'au',
+      '\u0935\u0902': 'am',
+      '\u0935\u0903': 'ah',
+
+      //numbers
+      '\u0966': '0',
+      '\u0967': '1',
+      '\u0968': '2',
+      '\u0969': '3',
+      '\u096a': '4',
+      '\u096b': '5',
+      '\u096c': '6',
+      '\u096d': '7',
+      '\u096e': '8',
+      '\u096f': '9',
+      //symbols
+      '%2c': ',',
+    };
